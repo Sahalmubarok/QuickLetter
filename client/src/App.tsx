7a44ab3149aee6b3ff455d4965120a98b1a0cd30
Sahalmubarok <sahalmubarok07@gmail.com>
2024-06-25 14:20:42 +0700

    twst

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Form from "./pages/Form";
+import DetailLetter from "./pages/DetailLetter";
 
 const App = () => {
   return (
     // <div>test</div>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/form" element={<Form />} />
+      <Route path="/detailletter" element={<DetailLetter />} />
     </Routes>
   );
 };
--- client/src/pages/Form/index.tsx
+++ client/src/pages/Form/index.tsx
@@ -5,42 +5,46 @@ import LetterOfStatement from "../../components/LetterOfStatement";
 import LetterRecipientAddres from "../../components/LetterRecipientAddres";
 import LetterContent from "../../components/LetterContent";
 import LetterSignature from "../../components/LetterSignature";
+import Navbar from "../../components/Navbar";
 
 const Form = () => {
   return (
-    <Box display={"flex"} flexDirection={"column"} height={"100vh"}>
-      <Box
-        display={"flex"}
-        justifyContent={"center"}
-        width={"100%"}
-        height={"10vh"}
-        sx={{ backgroundColor: "gray" }}
-      >
-        <Typography color={"white"}>ISI SURAT</Typography>
-      </Box>
-      <Box display={"flex"}>
-        <Box width={"50%"}>
-          <Box>
-            <LetterHead />
+    <>
+      <Navbar />
+      <Box mt={"120px"} display={"flex"} flexDirection={"column"} height={"100vh"}>
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          width={"100%"}
+          height={"10vh"}
+          sx={{ backgroundColor: "gray" }}
+        >
+          <Typography color={"white"}>ISI SURAT</Typography>
+        </Box>
+        <Box display={"flex"}>
+          <Box width={"50%"}>
+            <Box>
+              <LetterHead />
+            </Box>
+            <Box>
+              <LetterOfStatement />
+            </Box>
+            <Box>
+              <LetterRecipientAddres />
+            </Box>
+            <Box>
+              <LetterContent />
+            </Box>
+            <Box>
+              <LetterSignature />
+            </Box>
           </Box>
-          <Box>
-            <LetterOfStatement />
-          </Box>
-          <Box>
-            <LetterRecipientAddres />
-          </Box>
-          <Box>
-            <LetterContent />
-          </Box>
-          <Box>
-            <LetterSignature />
+          <Box width={"50%"}>
+            <Typography></Typography>
           </Box>
         </Box>
-        <Box width={"50%"}>
-          <Typography></Typography>
-        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
